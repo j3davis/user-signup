9ccec21ebd8fba94f697facf3818d386727841a6
John Davis <j3davis@gmail.com>
2019-04-13 17:01:23 -0500

    Beginning to write main

---
+++ main.py
@@ -0,0 +1,7 @@
+import cgi
+import os
+import jinja2
+template_dir = os.path.join(os.path.dirname(__file__),
+    'templates')
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(template_dir))    
